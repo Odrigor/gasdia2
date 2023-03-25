90984414f846018c21db801b32a749619e2c5195
Odrigor <45676199+Odrigor@users.noreply.github.com>
2023-03-25 12:17:29 -0300

    ordenando el proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,7 @@
 import {BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import LocationCheck from './LocationCheck'
 import './styles.css'
-import From2 from './From2'
-import MiPagina from './MiPagina'
-import Autocomplete from './autocomplete'
-import ValidatePosition from './ValidatePosition'
-import SearchAddress2 from './SearchAddress2'
-import  Alert2  from './Alertaaa'
 import { SolicitudProvider } from './Context/SolicitudProvider'
-import LocationComponent from './Components/LocationComponent'
-import FormularioInteligente from './FormularioInteligente'
+import FormularioInteligente from './Pages/FormularioInteligente'
 
 function App() {
 
@@ -20,12 +12,6 @@ function App() {
 <Router>
       <Routes>
         <Route path='/' element={<FormularioInteligente></FormularioInteligente>}></Route>
-        <Route path='/lo' element={<LocationCheck></LocationCheck>} ></Route>
-        <Route path='/autocomplete' element={<Autocomplete></Autocomplete>}></Route>
-        <Route path='/val' element={<ValidatePosition></ValidatePosition>}></Route>
-        <Route path='/s' element={<From2></From2>}></Route>
-        <Route path='/alert' element={<Alert2></Alert2>}></Route>
-        <Route path='/geolocation' element={<LocationComponent></LocationComponent>}></Route>
       </Routes>
     </Router>
 
